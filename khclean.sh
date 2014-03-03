5bcfd184cca8aa28593f7dd67792cf56d117adab
Leif Hedstrom <leif@ogre.com>
2014-03-03 11:22:29 +0000

    Added khclean.sh

---
+++ khclean.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# khclean.sh:  Simple script to clean out your known_hosts file
+# ----------   Copyright (C) 2014  Leif Hedstrom <leif@ogre.com>
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+HOSTS=$(ssh-keygen -l  -f ~/.ssh/known_hosts | grep "$*" | cut -d\  -f 3)
+
+for h in $HOSTS; do 
+    echo "Removing $h..."
+    ssh-keygen -R $h > /dev/null 2>&1
+done
